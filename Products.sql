6ed902ee5411f9723006a213157906f9c9dae462
Sandeep Shrestha <sandeepshresha41@gmail.com>
2024-05-01 16:15:26 +0545

    products updated with database

---
+++ Products.sql
@@ -0,0 +1,6 @@
+INSERT INTO `products` (`id`, `name`, `sku`, `price`, `description`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Product 1', 'SKU001', 100, 'Description 1', 'image1.jpg', '2024-04-01 02:15:00', '2024-04-01 02:15:00'),
+(2, 'Product 2', 'SKU002', 150, 'Description 2', 'image2.jpg', '2024-04-02 02:15:00', '2024-04-02 02:15:00'),
+(3, 'Product 3', 'SKU003', 200, 'Description 3', 'image3.jpg', '2024-04-03 02:15:00', '2024-04-03 02:15:00'),
+(4, 'Product 4', 'SKU004', 250, 'Description 4', 'image4.jpg', '2024-04-04 02:15:00', '2024-04-04 02:15:00'),
+(5, 'Product 5', 'SKU005', 300, 'Description 5', 'image5.jpg', '2024-04-05 02:15:00', '2024-04-05 02:15:00');
